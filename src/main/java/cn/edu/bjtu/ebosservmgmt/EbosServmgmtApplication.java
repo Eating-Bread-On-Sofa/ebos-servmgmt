09332b179813631279cf3c35afbc9fe64784df5e
Voo-C <voodoo.7@outlook.com>
2020-05-22 10:34:00 +0800

    first commit

---
+++ src/main/java/cn/edu/bjtu/ebosservmgmt/EbosServmgmtApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.bjtu.ebosservmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbosServmgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbosServmgmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/bjtu/ebosservmgmt/EbosServmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bjtu.ebosservmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbosServmgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
